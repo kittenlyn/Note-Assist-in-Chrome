7c7e2df81d4ebd59a590d50dca79cd6f4d60364d
Chiawen Lyn <chiawenlyn@gmail.com>
2015-05-31 15:51:13 +0900

    Add files

---
+++ js/bg.js
@@ -0,0 +1,20 @@
+﻿//Save the default menu to local storage
+if (!localStorage.isMenu) {
+  localStorage.isMenu = true;   //Activate
+  localStorage.allText="";
+ }
+ 
+function getText(info){
+	//chrome.tabs.create({url: "look.html"});
+	var txt=info.selectionText;
+	if(localStorage.allText!=""){
+	    localStorage.allText=localStorage.allText+"<br>"+txt;
+	}else{
+		localStorage.allText=txt;
+	}
+
+}
+chrome.contextMenus.create({"title": "★ Save the text(&A)", "type": "normal", contexts:["selection"], "onclick":getText});
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.create({url: "look.html"});
+})
---
+++ js/look.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function(e) {
+	var i=0;
+    var allText=localStorage.allText;
+	document.write(allText);
+	$("body").dblclick(function(){
+		if(i%2==0){
+			i=1;
+	        $(this).attr("contenteditable","true");	
+			$(this).css("color","#2E85BB");
+		}else{
+			i=0;
+			$(this).removeAttr("contenteditable");
+			$(this).css("color","#000");
+			localStorage.allText=$("body").html();
+		}
+	})
+});
